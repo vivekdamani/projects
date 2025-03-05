7f59503a5a52af35141ec4d20c48141c340cce59
Vivek Damani <56884865+vivekdamani@users.noreply.github.com>
2025-03-05 07:19:35 +0530

    Add files via upload

---
+++ Bike.java
@@ -0,0 +1,8 @@
+package ParkingLot.VehicleType;
+
+public class Bike extends Vehicle{
+    public Bike() {
+        this.size = 1;
+        this.type = VehicleType.BIKE;
+    }
+}
---
+++ Vehicle.java
@@ -0,0 +1,7 @@
+package ParkingLot.VehicleType;
+
+public abstract class Vehicle {
+    public int size;
+    public VehicleType type;    
+}
+
